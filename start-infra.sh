fe6218e35576c2897bbfab9b04acbf9bcdd665c7
Osvaldas Bernatavičius <Osvaldas.Bernatavicius@4finance.com>
2022-04-29 09:43:54 +0300

    Dependabot added.

--- start-infra.sh
+++ start-infra.sh
@@ -2,5 +2,7 @@
 
 docker-compose up -d
 
+sleep 1
+
 # Seed Vault
 curl -H "X-Vault-Token: super-secret-token" -H "Content-Type: application/json" -X POST -d @vault/application.json http://127.0.0.1:8200/v1/secret/data/application
