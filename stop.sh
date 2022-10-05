aa4df207768aeac97dc43f342cceaeac30b18167
Osvaldas <60891426+osber1@users.noreply.github.com>
2022-10-05 19:18:25 +0300

    Cucumber tests added.

--- stop.sh
+++ stop.sh
@@ -2,12 +2,8 @@
 
 VERSION="1.0-SNAPSHOT"
 
-docker rm -f loans-back-office
-docker rm -f loans-risk-checker
-docker rm -f loans-notification-service
+docker-compose -f docker-compose-full.yml down
 
 docker rmi loans-back-office:$VERSION
 docker rmi loans-risk-checker:$VERSION
 docker rmi loans-notification-service:$VERSION
-
-docker-compose down
