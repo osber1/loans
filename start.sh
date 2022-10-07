0344fd163970ba349893aca4230aa743d0eb471b
Osvaldas Bernatavičius <Osvaldas.Bernatavicius@4finance.com>
2022-10-07 12:38:49 +0300

    JIB added.

--- start.sh
+++ start.sh
@@ -5,10 +5,6 @@ VERSION="1.0-SNAPSHOT"
 # Need to have JAVA 17 installed.
 ./gradlew clean build --parallel
 
-docker build -f back-office/Dockerfile -t loans-back-office:$VERSION --build-arg VERSION=$VERSION back-office/build/libs
-docker build -f risk-checker/Dockerfile -t loans-risk-checker:$VERSION --build-arg VERSION=$VERSION risk-checker/build/libs
-docker build -f notification-service/Dockerfile -t loans-notification-service:$VERSION --build-arg VERSION=$VERSION notification-service/build/libs
-
 docker-compose -f docker-compose-full.yml up -d
 
 sleep 1
--- stop.sh
+++ stop.sh
@@ -4,6 +4,6 @@ VERSION="1.0-SNAPSHOT"
 
 docker-compose -f docker-compose-full.yml down
 
-docker rmi loans-back-office:$VERSION
-docker rmi loans-risk-checker:$VERSION
-docker rmi loans-notification-service:$VERSION
+docker rmi back-office:$VERSION
+docker rmi risk-checker:$VERSION
+docker rmi notification-service:$VERSION
