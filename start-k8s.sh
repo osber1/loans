c65db1450844736608ee137d2827e7411576a230
Osvaldas Bernatavičius <Osvaldas.Bernatavicius@4finance.com>
2022-12-05 22:05:57 +0200

    PostgreSQL and PGadmin added.

--- start-k8s.sh
+++ start-k8s.sh
@@ -4,12 +4,26 @@ VERSION="1.0-SNAPSHOT"
 
 # Need to have JAVA 17 installed.
 ./gradlew clean build --parallel
+docker tag risk-checker:$VERSION osvasldas97/risk-checker:$VERSION
+docker tag back-office:$VERSION osvasldas97/back-office:$VERSION
+docker tag notification-service:$VERSION osvasldas97/notification-service:$VERSION
+docker push osvasldas97/risk-checker:$VERSION
+docker push osvasldas97/back-office:$VERSION
+docker push osvasldas97/notification-service:$VERSION
 
-#minikube start --vm-driver kvm2
-kubectl apply -f k8s/postgresql/postgres.yaml
-kubectl apply -f k8s/postgresql/pg-admin.yaml
+# Uncomment only running for the first time
+#minikube config set memory 16384
+#minikube config set cpus 4
+#minikube config set vm-driver kvm2
+#minikube start
+#istioctl install -y
 
-sleep 1
+kubectl apply -f k8s
+#kubectl apply -f k8s/istio
+
+#sleep 1
 
 # Seed Vault
-curl -H "X-Vault-Token: super-secret-token" -H "Content-Type: application/json" -X POST -d @vault/application.json http://127.0.0.1:8200/v1/secret/data/application
+#curl -H "X-Vault-Token: super-secret-token" -H "Content-Type: application/json" -X POST -d @vault/application.json http://127.0.0.1:8200/v1/secret/data/application
+
+minikube service list
--- stop-k8s.sh
+++ stop-k8s.sh
@@ -2,10 +2,13 @@
 
 VERSION="1.0-SNAPSHOT"
 
-kubectl delete -f k8s/postgresql/postgres.yaml
-kubectl delete -f k8s/postgresql/pg-admin.yaml
+kubectl delete -f k8s
+kubectl delete -f k8s/istio
 #minikube delete
 
 docker rmi back-office:$VERSION
 docker rmi risk-checker:$VERSION
 docker rmi notification-service:$VERSION
+docker rmi osvasldas97/back-office:$VERSION
+docker rmi osvasldas97/risk-checker:$VERSION
+docker rmi osvasldas97/notification-service:$VERSION
