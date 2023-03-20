b124cb56f99e9c0d4abcfe1b779d54977d77c2ea
Osvaldas Bernatavičius <Osvaldas.Bernatavicius@4finance.com>
2023-03-20 12:06:21 +0200

    Libraries updated.

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ VERSION="1.0-SNAPSHOT"
 
 docker-compose -f docker-compose.yml -f docker-compose-applications.yml up -d
 
-./infra_config/vault/seed_vault.sh
+./infra_config/vault/seed_vault.sh localhost 8200
 ./infra_config/kibana/seed_kibana.sh
