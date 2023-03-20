823daa156e4a7d3ba90c7b0d55a0979fa19fa9bf
Osvaldas Bernatavičius <Osvaldas.Bernatavicius@4finance.com>
2023-03-20 13:57:30 +0200

    Libraries updated.

--- start.sh
+++ start.sh
@@ -5,7 +5,10 @@ VERSION="1.0-SNAPSHOT"
 # Need to have JAVA 17 installed.
 ./gradlew clean build --parallel
 
-docker-compose -f docker-compose.yml -f docker-compose-applications.yml up -d
+docker-compose up -d
 
 ./infra_config/vault/seed_vault.sh localhost 8200
+
+docker-compose -f docker-compose-applications.yml up -d
+
 ./infra_config/kibana/seed_kibana.sh
