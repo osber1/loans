afa2c4cc17fb915eb4f5dfe58e34fb08aaa7301e
Osvaldas Bernatavičius <Osvaldas.Bernatavicius@4finance.com>
2023-08-01 10:16:05 +0300

    Error prone updated.

--- back-office/src/main/java/io/osvaldas/backoffice/repositories/specifications/LoanSpecifications.java
+++ back-office/src/main/java/io/osvaldas/backoffice/repositories/specifications/LoanSpecifications.java
@@ -12,21 +12,23 @@ import io.osvaldas.backoffice.repositories.entities.Client_;
 import io.osvaldas.backoffice.repositories.entities.Loan;
 import io.osvaldas.backoffice.repositories.entities.Loan_;
 
-@SuppressWarnings("InterfaceWithOnlyStatics")
-public interface LoanSpecifications {
+public final class LoanSpecifications {
 
-    static Specification<Loan> clientIdIs(String clientId) {
+    private LoanSpecifications() {
+    }
+
+    public static Specification<Loan> clientIdIs(String clientId) {
         return (root, query, cb) -> {
             Join<Loan, Client> clients = root.join(Loan_.CLIENT);
             return cb.equal(clients.get(Client_.ID), clientId);
         };
     }
 
-    static Specification<Loan> loanCreationDateIsAfter(ZonedDateTime date) {
+    public static Specification<Loan> loanCreationDateIsAfter(ZonedDateTime date) {
         return (root, query, cb) -> cb.greaterThan(root.get(Loan_.CREATED_AT), date);
     }
 
-    static Specification<Loan> loanStatusIs(Status status) {
+    public static Specification<Loan> loanStatusIs(Status status) {
         return (root, query, cb) -> cb.equal(root.get(Loan_.STATUS), status);
     }
 
