8ec38d443d8520db8f8b4ece64425fc7d0036495
Osvaldas Bernatavičius <Osvaldas.Bernatavicius@4finance.com>
2023-08-07 23:46:08 +0300

    Updated config

--- start-k8s.sh
+++ start-k8s.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-VERSION="1.0-SNAPSHOT"
-
-# Need to have JAVA 17 installed.
 ./gradlew clean build
 
-# Uncomment only running for the first time
-minikube start --memory 16384 --cpus=4 --driver=kvm2
+minikube start --memory 20000 --cpus=6 --driver=kvm2
 minikube addons enable ingress
 istioctl install -y
 
-sleep 10
+sleep 20
 
 kubectl apply -f k8s/infra
 kubectl apply -f k8s/istio
--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-VERSION="1.0-SNAPSHOT"
-
-# Need to have JAVA 17 installed.
 ./gradlew clean build
 
 docker-compose up -d
