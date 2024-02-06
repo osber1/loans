7d1af5b6c4ddbd2a94716a30f67736bd443d4b22
Osvaldas Bernatavičius <osvaldas.bernatavicius@4finance.com>
2024-02-06 10:58:08 +0200

    ProblemDetail added to exception handling.

--- api/src/main/java/io/osvaldas/api/exceptions/ApiRequestException.java
+++ api/src/main/java/io/osvaldas/api/exceptions/ApiRequestException.java
@@ -1,15 +1,6 @@
 package io.osvaldas.api.exceptions;
 
-import static java.time.ZonedDateTime.now;
-
-import java.time.ZonedDateTime;
-
 import org.springframework.http.HttpStatus;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 public abstract class ApiRequestException extends RuntimeException {
 
@@ -19,25 +10,4 @@ public abstract class ApiRequestException extends RuntimeException {
 
     public abstract HttpStatus getHttpStatus();
 
-    public ApiExceptionResource toResource() {
-        return toResource(getMessage());
-    }
-
-    public ApiExceptionResource toResource(String message) {
-        return new ApiExceptionResource(message, now(), getHttpStatus());
-    }
-
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ApiExceptionResource {
-
-        private String message;
-
-        private ZonedDateTime timestamp;
-
-        private HttpStatus status;
-
-    }
 }
--- back-office/src/main/java/io/osvaldas/backoffice/infra/exception/ApiExceptionHandler.java
+++ back-office/src/main/java/io/osvaldas/backoffice/infra/exception/ApiExceptionHandler.java
@@ -1,6 +1,6 @@
 package io.osvaldas.backoffice.infra.exception;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,8 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 public class ApiExceptionHandler {
 
     @ExceptionHandler(ApiRequestException.class)
-    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+    public ProblemDetail handleApiRequestException(ApiRequestException e) {
         log.error(e.getMessage(), e);
-        return new ResponseEntity<>(e.toResource(), e.getHttpStatus());
+        return ProblemDetail.forStatusAndDetail(e.getHttpStatus(), e.getMessage());
     }
+
 }
