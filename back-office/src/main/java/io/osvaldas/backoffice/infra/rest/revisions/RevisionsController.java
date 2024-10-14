311c24ec3ebd6699fdf9111d4aed66feef244b32
Osvaldas Bernatavičius <osvaldas.bernatavicius@4finance.com>
2024-10-14 13:56:34 +0300

    refactor(revisions): replace Hibernate Envers with Spring Data Revisions
    Replaced Hibernate Envers with Spring Data Revisions in RevisionsController. Updated LoanRepository to extend RevisionRepository. This change simplifies the retrieval of entity

--- back-office/src/main/java/io/osvaldas/backoffice/infra/rest/revisions/RevisionsController.java
+++ back-office/src/main/java/io/osvaldas/backoffice/infra/rest/revisions/RevisionsController.java
@@ -2,9 +2,7 @@ package io.osvaldas.backoffice.infra.rest.revisions;
 
 import java.util.List;
 
-import org.hibernate.envers.AuditReader;
-import org.hibernate.envers.AuditReaderFactory;
-import org.hibernate.envers.query.AuditEntity;
+import org.springframework.data.history.Revision;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.osvaldas.api.clients.ClientResponse;
 import io.osvaldas.api.loans.LoanResponse;
-import io.osvaldas.backoffice.repositories.entities.Client;
-import io.osvaldas.backoffice.repositories.entities.Loan;
+import io.osvaldas.backoffice.repositories.ClientRepository;
+import io.osvaldas.backoffice.repositories.LoanRepository;
 import io.osvaldas.backoffice.repositories.mapper.ClientMapper;
 import io.osvaldas.backoffice.repositories.mapper.LoanMapper;
-import jakarta.persistence.EntityManagerFactory;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -24,36 +21,26 @@ import lombok.AllArgsConstructor;
 @RequestMapping("api/v1/revisions")
 public class RevisionsController {
 
-    private final EntityManagerFactory factory;
+    private final ClientRepository clientRepository;
 
     private final ClientMapper clientMapper;
+
+    private final LoanRepository loanRepository;
 
     private final LoanMapper loanMapper;
 
     @GetMapping("clients/{clientId}")
     public List<ClientResponse> getClientRevisions(@PathVariable String clientId) {
-        AuditReader reader = AuditReaderFactory.get(factory.createEntityManager());
-        List<Client> resultList = reader.createQuery()
-            .forRevisionsOfEntity(Client.class, true, true)
-            .add(AuditEntity.id().eq(clientId))
-            .getResultList();
-
-        return resultList
-            .stream()
+        return clientRepository.findRevisions(clientId).stream()
+            .map(Revision::getEntity)
             .map(clientMapper::map)
             .toList();
     }
 
     @GetMapping("loans/{loanId}")
     public List<LoanResponse> getLoansRevisions(@PathVariable long loanId) {
-        AuditReader reader = AuditReaderFactory.get(factory.createEntityManager());
-        List<Loan> resultList = reader.createQuery()
-            .forRevisionsOfEntity(Loan.class, true, true)
-            .add(AuditEntity.id().eq(loanId))
-            .getResultList();
-
-        return resultList
-            .stream()
+        return loanRepository.findRevisions(loanId).stream()
+            .map(Revision::getEntity)
             .map(loanMapper::map)
             .toList();
     }
