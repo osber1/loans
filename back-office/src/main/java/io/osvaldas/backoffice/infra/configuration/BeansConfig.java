8bb4b863d2a8bac304c3efe82dc3a45b862c993a
Osvaldas Bernatavičius <osvaldas.bernatavicius@4finance.com>
2026-01-23 21:13:07 +0200

    CacheManager bean added and removed open-in-view setting.

--- back-office/src/main/java/io/osvaldas/backoffice/infra/configuration/BeansConfig.java
+++ back-office/src/main/java/io/osvaldas/backoffice/infra/configuration/BeansConfig.java
@@ -9,6 +9,8 @@ import static org.springframework.data.redis.serializer.RedisSerializationContex
 
 import javax.sql.DataSource;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -59,6 +61,11 @@ public class BeansConfig {
         threadPoolTaskScheduler.setPoolSize(3);
         threadPoolTaskScheduler.setThreadNamePrefix("cronJobThreadPoolTaskScheduler");
         return threadPoolTaskScheduler;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("LoanResponse");
     }
 
 }
